0fe79bef5507c20ef174cfa5a1d93186169eda84
ogbotemi-2000 <oogbotemi@gmail.com>
2025-02-15 00:31:08 -0800

    committing vendor scripts few at a time

--- public/index.php
+++ public/index.php
@@ -7,13 +7,13 @@ define('LARAVEL_START', microtime(true));
 $dir = dirname(__DIR__, 1);
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = $dir.'/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require $dir.'/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once $dir.'/bootstrap/app.php')
+(require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
